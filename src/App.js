bec731147b610caecc5a2c62fc8661eddd009f8b
Heberth Junor <hjunor@gmail.com>
2021-10-17 22:06:45 -0300

    add: added the  manifest.json

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import { Storage } from "./context/storeContext";
 import Routes from "./routes";
 import Headers from "./components/header";
 import MenuMobile from "./components/MemuMobile";
+import PWAPrompt from "react-ios-pwa-prompt";
+
 const App = () => (
   <Storage>
+    <PWAPrompt
+      copyTitle="iClearchain this app is from http://aurionsystems.com.au/ "
+      copyBody="This application has app functionality. Add it to your home screen to use it in fullscreen and while offline "
+      promptOnVisit={1}
+      timesToShow={3}
+      copyClosePrompt="Close"
+      permanentlyHideOnDismiss={false}
+    />
     <BrowserRouter>
       <Headers />
       <Routes />
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/global.scss";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-serviceWorkerRegistration.register();
+serviceWorkerRegistration();
